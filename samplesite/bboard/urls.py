6d75f1c19e474e457d46ffdb225f7b38e3e46bd3
LlamaUnicorn <holidaynight@gmail.com>
2023-02-01 07:46:00 +0300

    Added URL reversing and started forms

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, by_rubric
 
 urlpatterns = [
-    path('', index),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('', index, name='index'),
 ]
