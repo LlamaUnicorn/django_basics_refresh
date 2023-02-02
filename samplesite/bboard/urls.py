6df90da813c8005be435db062d184ad14291321b
LlamaUnicorn <holidaynight@gmail.com>
2023-02-02 09:08:19 +0300

    Finished form to add posts

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index, by_rubric
+from .views import index, by_rubric, BbCreateView
 
 urlpatterns = [
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('add/', BbCreateView.as_view(), name='add'),
     path('', index, name='index'),
 ]
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 from .models import Bb
 from .models import Rubric
+from .forms import BbForm
 
 
 def index(request):
@@ -17,3 +20,14 @@ def by_rubric(request, rubric_id):
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
     return render(request, 'bboard/by_rubric.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
